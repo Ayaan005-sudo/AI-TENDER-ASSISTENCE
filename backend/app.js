b779f445ea4884e6e85865d226b8539cf8ae117d
Ayaan Ahmed Abbasi <ayaanah776@gmail.com>
2026-06-24 10:26:28 +0530

    inital setup for backend done

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require("express");
+const app =express();
+
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(express.json());
+app.use(cors());
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_URI);
+}
+main()
+.then(console.log("mongoose get connected"))
+.catch((e)=>console.log(e.message))
+
+
+
+const port = process.env.PORT||3000;
+app.listen(port,()=>{
+    console.log("app is listening to port");
+})
+
